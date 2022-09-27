d6b1e7e3917daaee0e1be29d213316c364e6c0aa
edellirom <dellirom.epam@gmail.com>
2022-09-27 19:31:33 +0300

    Integration with S3

---
+++ import-service/src/services/import-file-parser/create-read-stream.ts
@@ -0,0 +1,26 @@
+import AWS from 'aws-sdk';
+import { Readable } from 'stream';
+
+export default async function createReadStream(
+  s3: AWS.S3,
+  s3BucketName: string,
+  s3BucketKey: string
+): Promise<Readable> {
+  return new Promise((resolve, reject) => {
+    const params = {
+      Bucket: s3BucketName,
+      Key: s3BucketKey,
+    };
+    try {
+      s3.headObject(params, (error, data) => {
+        if (error) {
+          throw error;
+        }
+        const stream = s3.getObject(params).createReadStream();
+        resolve(stream);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
